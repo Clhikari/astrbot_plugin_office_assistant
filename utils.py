05141ea729b1d4810ab20fdbced3be010d137b70
Clhikari <1401333620@qq.com>
2025-12-16 12:50:52 +0800

    工具函数

---
+++ utils.py
@@ -0,0 +1,8 @@
+
+def format_file_size(size: int | float) -> str:
+    """格式化文件大小"""
+    for unit in ["B", "KB", "MB", "GB"]:
+        if size < 1024:
+            return f"{size:.2f} {unit}"
+        size /= 1024
+    return f"{size:.2f} TB"
